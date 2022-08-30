8ecbb7c17322c3807162e686fb38202afe3ad531
Cristina Rodriguez <yosola@gmail.com>
2022-08-30 10:35:24 -0700

    Working using async and rendering static files

--- restapi/index.js
+++ restapi/index.js
@@ -8,6 +8,9 @@ import pretty from 'express-prettify';
 const app = express();
 const PORT = 5000;
 
+// to let the server know what directory are we working on 
+const __dirname = path.resolve();
+
 // Configuring cors middleware
 app.use(cors());
 
@@ -18,17 +21,22 @@ app.use(pretty({ query: 'pretty' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//creates an endpoint for the route `/` that is the homepage
-app.get('/', (req, res) =>{
-    //console.log("[PRUEBA]!");
-    res.send("Hello Techtonica, welcome to Cristina's Library");
-}); 
+// to render static files from the client folder
+app.use(express.static('client'));
 
 
 // //creates an endpoint for the route `/api/books` that prints all the books 
 app.get("/api/books", (req, res) => {
     res.json(books);
   });
+
+//creates a route `/` that is the homepage
+app.get('/', (req, res) =>{
+  //testing that was working
+  //res.send("Hello Techtonica, welcome to Cristina's Library");
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+  //this send the response to open the index.html in that directory
+}); 
 
 
 app.listen(PORT, () => console.log(`Hola! Server running on Port http://localhost:${PORT}`));
